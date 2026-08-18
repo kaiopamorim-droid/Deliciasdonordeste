876bdd1886654426577c3997896f46e5430d567a
kaio <kaiopamorim@gmail.com>
2026-08-18 20:06:43 -0300

    corrige jsx quebrado no Instagram

--- src/sections/Instagram.jsx
+++ src/sections/Instagram.jsx
@@ -1,33 +1,16 @@
-import { marca } from '../data/site.js'
 import { instagramPosts } from '../data/conteudo.js'
-import { Botao, Figura, Revelar, TituloSecao } from '../components/Primitivos.jsx'
+import { Figura, Revelar, TituloSecao } from '../components/Primitivos.jsx'
 
 export default function Instagram() {
   return (
     <section aria-labelledby="titulo-instagram" className="mt-24 lg:mt-36">
       <div className="mx-auto max-w-conteudo px-5 sm:px-8">
-        <div className="flex flex-col gap-6 sm:flex-row sm:items-end sm:justify-between">
-          <Revelar>
-            <TituloSecao
-              etiqueta={marca.instagram.usuario}
-              titulo="Acompanhe a Delícias do Nordeste"
-            />
-            <span id="titulo-instagram" className="sr-only">
-              Instagram
-            </span>
-          </Revelar>
-
-          <Revelar atraso={100}>
-            <Botao
-              href={marca.instagram.url}
-              variante="contorno"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Ver Instagram
-            </Botao>
-          </Revelar>
-        </div>
+        <Revelar>
+          <TituloSecao titulo="Mais produtos" />
+          <span id="titulo-instagram" className="sr-only">
+            Instagram
+          </span>
+        </Revelar>
 
         <div className="mt-12 grid grid-cols-2 gap-3 sm:grid-cols-3 lg:grid-cols-6">
           {instagramPosts.map((post, i) => (
